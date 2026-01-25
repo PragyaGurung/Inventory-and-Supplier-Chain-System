e57df5624955c39f28f87c2ee4f1627c1602bf62
Pragya <pragyagurung1221@gmail.com>
2026-01-25 22:27:44 +0545

    Add: Module file

---
+++ Models/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_and_Supplier_Chain_System.Models
+{
+    internal class Purchase
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int SupplierId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string Notes { get; set; }
+
+        public Purchase()
+        {
+            PurchaseDate = DateTime.Now;
+        }
+    }
+}
