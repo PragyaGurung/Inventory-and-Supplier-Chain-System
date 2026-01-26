0c93b5237b7bf4207898a79ddaa1ccd381bc2bcf
Pragya <pragyagurung1221@gmail.com>
2026-01-26 15:36:17 +0545

    Added: PurchaseService code(re-write)

--- Services/InventoryService.cs
+++ Services/InventoryService.cs
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
-﻿using Inventory_and_Supplier_Chain_System.Models;
-=======
+using Inventory_and_Supplier_Chain_System.Models;
 ﻿using Inventory_and_Supplier_Chain_System.Data;
->>>>>>> 547e0a306327490fb41f2b408d605a0807fc016c
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Inventory_and_Supplier_Chain_System.Services
 {
-<<<<<<< HEAD
+
     public class InventoryService
     {
         public bool UpdateProduct(Product product)
@@ -49,12 +46,6 @@ namespace Inventory_and_Supplier_Chain_System.Services
                 return false;
             }
         }
-=======
-    public class InventoryService 
-    {
-    
->>>>>>> 547e0a306327490fb41f2b408d605a0807fc016c
-
     }
    
 }
